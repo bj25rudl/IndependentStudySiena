3ebe4c7f1f23eaf8c3a060c18fed5974234b2c71
Brett <brettrudloff@brett-rudloffs-macbook-pro.local>
2012-11-04 16:09:17 -0500

    Two Buttons app

---
+++ TwoButtons/Two Buttons/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Two Buttons
+//
+//  Created by Jessica Reinhardt on 11/2/12.
+//  Copyright (c) 2012 Jessica Reinhardt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TwoButtons/Two Buttons/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Two Buttons
+//
+//  Created by Jessica Reinhardt on 11/2/12.
+//  Copyright (c) 2012 Jessica Reinhardt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
